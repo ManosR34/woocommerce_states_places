1b9abac22af0545a33a3fc1d4156fb0f59d93b33
ManosR34 <44647854+ManosR34@users.noreply.github.com>
2020-11-21 11:02:51 +0200

    Update and rename XX.php to GR1.php

---
+++ templates/states/GR1.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * $country states/provinces/regions
+ * - $count states/provinces/regions
+ * 
+ * Source: 
+ * - [Please add the source(s) link(s) to check the list of places]
+ *
+ * @author  Manos Romanos <manosR34@gmail.com> 
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $states;
+
+// Replace 'XX' code for your Country Code. E.g. 'NG' for Nigeria, 'CO' for Colombia, 'VE' for Venezuela, etc.
+// Please refer to WooCommerce country codes : https://github.com/woocommerce/woocommerce/blob/master/i18n/countries.php
+$states['GR'] = array(
+  // Add a State Code to each State Name using a logic sense. 
+  // E.g. 'FA' => 'Florida'
+  // You can use any standar like ISO, ANSI, etc. There is no an mandatory standar or system and you can use your own.
+  // Please see the U.S. state abbreviations to see an example:
+  // https://en.wikipedia.org/wiki/List_of_U.S._state_abbreviations
+  'AN' => 'Αιτωλοακαρνανίας',
+  'AG' => 'Αργολίδας',
+  'AK' => 'Αρκαδίας',
+  'AR' => 'Άρτας',
+  'AT' => 'Αττικής',
+  'AX' => 'Αχαίας',
+  'VI' => 'Βοιωτίας',
+  'GV' => 'Γρεβενών',
+  'DR' => 'Δράμας',
+  'DO' => 'Δωδεκανήσου',
+  'EV' => 'Έβρου',
+  'EO' => 'Ευβοίας',
+  'ER' => 'Ευρυτανίας',
+  'ZA' => 'Ζακύνθου',
+  'IL' => 'Ηλείας',
+  'IM' => 'Ημαθίας',
+  'IR' => 'Ηρακλείου',
+  'TH' => 'Θεσπρωτίας',
+  'TS' => 'Θεσσαλονίκης',
+  'JO' => 'Ιωαννίνων',
+  'KV' => 'Καβάλας',
+  'KR' => 'Καρδίτσας',
+  'KS' => 'Καστοριάς',
+  'KE' => 'Κέρκυρας',
+  'KF' => 'Κεφαλληνίας',
+  'KI' => 'Κιλκίς',
+  'KZ' => 'Κοζάνης',
+  'KO' => 'Κορίνθου',
+  'KY' => 'Κυκλάδων',
+  'LK' => 'Λακωνίας',
+  'LA' => 'Λαρίσης',
+  'LS' => 'Λασιθίου',
+  'LE' => 'Λέσβου',
+  'LF' => 'Λευκάδας',
+  'MG' => 'Μαγνησίας',
+  'MS' => 'Μεσσηνίας',
+  'XA' => 'Ξάνθης',
+  'PE' => 'Πέλλας',
+  'PI' => 'Πιερίας',
+  'PR' => 'Πρέβεζας',
+  'RE' => 'Ρεθύμνου',
+  'RO' => 'Ροδόπης',
+  'SA' => 'Σάμου',
+  'SE' => 'Σερρών',
+  'TR' => 'Τρικάλων',
+  'FT' => 'Φθιώτιδας',
+  'FL' => 'Φλώρινας',
+  'FO' => 'Φωκίδας',
+  'HL' => 'Χαλκιδικής',
+  'HA' => 'Χανίων',
+  'HI' => 'Χίου',
+);
